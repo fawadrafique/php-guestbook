6a50f3d13675d99db1b69ab62969075ada64afbd
Muhammad Fawad Rafique <fawadrafique@hotmail.com>
2020-09-29 21:24:56 +0200

    Split index file to seperate files

--- View/index.php
+++ View/index.php
@@ -1,44 +1,43 @@
+<?php
+declare(strict_types=1);
+
+include '../Model/post.php';
+include '../Controller/controller.php';
+
+session_start();
+whatIsHappening();
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SERVER["REQUEST_URI"]);
+
+}
+//print_r($post->getPost());
+?>
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Guestbook</title>
-</head>
-<body class="bg-light">
-<div class="container">
-    <div class="py-5 text-center">
-<!--        <img class="d-block mx-auto mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">-->
-        <h2>Digital Guestbook</h2>
-        <p class="lead">Write something to your friend!.</p>
-    </div>
-    <form>
+
+<?php include 'header.php'; ?>
+
+    <form action="book.php" method="post">
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="name" class="form-control" id="name" placeholder="Name (required)">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Name (required)">
         </div>
         <div class="form-group">
-            <label for="exampleFormControlTextarea6">Comment</label>
-            <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Write something here..."></textarea>
+            <label for="comment">Comment</label>
+            <textarea name="comment" class="form-control z-depth-1" rows="3" placeholder="Write something here..."></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button action="submit" name="submit" value="1" class="btn btn-primary">Submit</button>
     </form>
-    <div class="card border  mt-3">
-    <div class="card-header bg-info text-light">
-        <h3 class="h3 mb-0">
-            Fawad
-        </h3>05/12/2019 03:37:41
-    </div>
-    <div class="card-body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
-    </div>
-    </div>
-</div>
 
-</body>
+<?php include 'footer.php'; ?>
+
 </html>
 
